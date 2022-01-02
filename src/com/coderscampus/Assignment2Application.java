8593c422501c9c96a6eb916223ef8c0106368d54
rimpy106 <rimpy106@gmail.com>
2022-01-02 01:50:14 -0600

    Update Higher/Lower Guessing Game Code

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,39 @@
 package com.coderscampus;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random random = new Random();
+		int randomNum = random.nextInt(100);
+		System.out.println("randomNum = " + randomNum);
+		Scanner scanner = new Scanner(System.in);
 
+		boolean gameWon = false;
+		int guessNum = 0;
+		while (guessNum < 5) {
+			System.out.println("Pick a number between 1 and 100");
+			int inputNum = scanner.nextInt();
+			if (inputNum < 1 || inputNum > 100) {
+				System.out.println("Your guess is not between 1 and 100, please try again");
+			} else {
+				if (randomNum > inputNum) {
+					System.out.println("Please pick a higher number");
+					guessNum++;
+				} else if (randomNum < inputNum) {
+					System.out.println("Please pick a lower number");
+					guessNum++;
+				} else {
+					gameWon = true;
+					System.out.println("You win!");
+					break;
+				}
+			}
+		}
+		if (!gameWon)
+			System.out.println("You lose, the number to guess was : " + randomNum);
 	}
-
 }
