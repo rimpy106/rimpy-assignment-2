34b8d3a89865423328bae53e89c441333ea2b1fd
rimpy106 <rimpy106@gmail.com>
2022-01-07 15:05:07 -0600

    UpdateCode

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -8,7 +8,7 @@ public class Assignment2Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Random random = new Random();
-		int randomNum = random.nextInt(100);
+		int randomNum = random.nextInt(100) + 1;
 		System.out.println("randomNum = " + randomNum);
 		Scanner scanner = new Scanner(System.in);
 
@@ -20,19 +20,19 @@ public class Assignment2Application {
 			if (inputNum < 1 || inputNum > 100) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 			} else {
-				if (randomNum > inputNum) {
-					System.out.println("Please pick a higher number");
-					guessNum++;
-				} else if (randomNum < inputNum) {
-					System.out.println("Please pick a lower number");
-					guessNum++;
-				} else {
+				if (randomNum == inputNum) {
 					gameWon = true;
 					System.out.println("You win!");
 					break;
+				} else if (randomNum > inputNum) {
+					System.out.println("Please pick a higher number");
+				} else if (randomNum < inputNum) {
+					System.out.println("Please pick a lower number");
 				}
+				guessNum++;
 			}
 		}
+		scanner.close();
 		if (!gameWon)
 			System.out.println("You lose, the number to guess was : " + randomNum);
 	}
